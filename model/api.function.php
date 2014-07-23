d2740b7e3a4b4f38eac66ffe20b773ea10bcd821
lux <lux@knownsec.com>
2014-07-23 15:56:09 +0800

    修补添加TODO的XSS漏洞

--- model/api.function.php
+++ model/api.function.php
@@ -132,6 +132,7 @@ function add_todo( $text , $is_public = 0 , $uid = null )
 {
 	if( $uid == null || intval($uid) < 1 ) $uid = $_SESSION['uid'];
 	
+    $text = htmlspecialchars($text, ENT_QUOTES|ENT_XHTML, 'UTF-8');
 	$sql = "INSERT INTO `todo` ( `content` ,  `timeline` , `owner_uid` ) VALUES ( '" . s( $text ) . "'  , NOW() , '" . intval( $uid ) . "' ) ";
 	run_sql( $sql );
 	
